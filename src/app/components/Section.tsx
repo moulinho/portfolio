e90d92d3e4cd698da88201d6b064ae0c471f4909
sereme <seremeabd@gmail.com>
2026-04-30 10:13:00 +0000

    add cv optimise

--- src/app/components/Section.tsx
+++ src/app/components/Section.tsx
@@ -6,6 +6,23 @@ type SectionProps = {
   children: ReactNode;
   id?: string;
 };
+
+type CvDownloadProps = {
+  href: string;
+  children: ReactNode;
+};
+
+function CvDownload({ href, children }: CvDownloadProps) {
+  return (
+    <Link
+      href={href}
+      target="_blank"
+      className="bg-black text-white px-8 py-4 rounded-md hover:bg-gray-800 transition-all"
+    >
+      {children}
+    </Link>
+  );
+}
 const Section = ({ title, children, id }: SectionProps) => (
   <section className="my-5 text-base/7" id={id}>
     <div className="flex items-center justify-between mb-4">
@@ -13,23 +30,17 @@ const Section = ({ title, children, id }: SectionProps) => (
     <span className="text-gray-500 text-sm">
       
       {title.includes("propos") && (
-        <Link
-          href="https://drive.google.com/file/d/1QwfUSYlrkOiODwUz6zTdkxpAGK30GguI/view?usp=sharing"
-          target="_blank"
-          className="bg-black text-white px-8 py-4 rounded-md hover:bg-gray-800 transition-all"
-        >
+        <CvDownload href="https://drive.google.com/file/d/1QwfUSYlrkOiODwUz6zTdkxpAGK30GguI/view?usp=sharing">
           Télécharger mon CV
-        </Link>
+        </CvDownload>
       )}
+       
       {title.includes("About") && (
-        <Link
-          href="https://docs.google.com/document/d/1zpzvsBe7vHZe8_2B3ijHoIpgt5mfH3Z1/edit?usp=sharing&ouid=117967692092447813414&rtpof=true&sd=true"
-          target="_blank"
-          className="bg-black text-white px-8 py-4 rounded-md hover:bg-gray-800 transition-all"
-        >
+        <CvDownload href="https://docs.google.com/document/d/1zpzvsBe7vHZe8_2B3ijHoIpgt5mfH3Z1/edit?usp=sharing&ouid=117967692092447813414&rtpof=true&sd=true">
           Download my CV
-        </Link>
+        </CvDownload>
       )}
+         
     </span>
     </div>
     {children}
